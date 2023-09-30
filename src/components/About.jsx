0d2ea003bad2b13c98d1748b9af99e4de47ccb0d
Israel Oluwagbogo <israeladeyemi489@gmail.com>
2023-09-30 10:51:42 +0100

    doned

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,15 +4,15 @@ const About = () => {
     <div className="w-full bg-brand py-6 px-6 sm:px-20 flex sm:flex-row flex-col items-center justify-center gap-5">
       <div className="flex flex-col gap-7 basis-1/2">
         <p className="text-yellow-400 text-2xl font-bold">
-          About Smart Deployer
+          About Tokenfy Launcher
         </p>
         <p className="text-white text-xl font-normal w-full">
-          Tokenfy launcher is a customizable platform where tokens can be
-          created and made easily customizable for token creators to launch
-          their tokens. With our core features lies the possibility for our
-          users (businesses, communities, organizations and companies) to manage
-          their tokens directly through our platform, import tokens, and
-          customize tokens as preferable.
+          Tokenfy Launcher is a customizable platform that enables easy creation
+          and customization of tokens for token creators looking to launch their
+          projects. Our core features empower users, including businesses,
+          communities, organizations, and companies, to manage their tokens
+          directly through our platform. Users can import tokens and customize
+          them according to their preferences.
         </p>
         <button
           className={`rounded-md p-2 w-fit active:scale-95 transition-all duration-300 font-medium bg-white color px-3`}
